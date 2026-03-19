c90d9c9d819a23081d990abef3847c84a0988f93
Heitor-Cuba <heitorcuba@gmail.com>
2026-03-19 13:13:48 -0300

    Criei o EditorConteudo (inicialmente inputLabel)

---
+++ src/components/inputLabel.tsx
@@ -0,0 +1,11 @@
+export function InputLabel({value}: {value: string}) {
+    return (
+        <div
+            style={{
+                fontSize: '100px',
+            }}
+        >
+            {value}
+        </div>
+    )
+}
